7b900b2c9ad19ff2aebb3559c2f7b972a5e1aa7e
Alice <a.aliceabraham@gmail.com>
2023-12-21 14:39:12 +0100

    crud user

--- src/classes/user.ts
+++ src/classes/user.ts
@@ -10,4 +10,111 @@ export class User {
         this.name = name;
         this.email = email;
     }
+
+    public static async getAllUsers(): Promise<User[]> {
+        try {
+            const users = await prisma.user.findMany();
+            return users.map((user) => ({
+                id: user.id,
+                name: user.name,
+                email: user.email
+            }));
+        } catch (error) {
+            console.error("Error fetching users:", error);
+            throw new Error("Failed to fetch users");
+        }
+    }
+    public static async getUser(userId: number): Promise<User | null> {
+        try {
+            const existingUser = await prisma.user.findUnique({
+                where: {
+                    id: userId,
+                },
+            });
+            if (existingUser) {
+                return {
+                    id: existingUser.id,
+                    name: existingUser.name,
+                    email: existingUser.email
+                };
+            } else {
+                console.error(`User with ID ${userId} not found.`);
+                return null;
+            }
+        } catch (error) {
+            console.error("Error creating user:", error);
+            throw new Error("Failed to create user");
+        }
+    }
+    public static async createUser(name: string, email: string): Promise<User> {
+        try {
+            const newUser = await prisma.user.create(
+                {
+                    data: {
+                        name: name,
+                        email: email,
+                    }
+                });
+            return {
+                id: newUser.id,
+                name: newUser.name,
+                email: newUser.email,
+            };
+        } catch (error) {
+            console.error("Error creating user:", error);
+            throw new Error("Failed to create user");
+        }
+    }
+    public static async updateUser(id: number, name: string, email: string): Promise<User | null> {
+        try {
+            const existingUser = await prisma.user.findUnique({
+                where: {
+                    id: id,
+                },
+            });
+            if (existingUser) {
+                const updatedUser = await prisma.user.update({
+                    where: {
+                        id: id,
+                    },
+                    data: {
+                        name: name,
+                        email: email,
+                    },
+                });
+                return {
+                    id: updatedUser.id,
+                    name: updatedUser.name,
+                    email: updatedUser.email,
+                };
+            } else {
+                console.error(`User with ID ${id} not found.`);
+                return null;
+            }
+        } catch (error) {
+            console.error("Error updating user:", error);
+            throw new Error("Failed to update user");
+        }
+    }
+    public static async deleteUser(userId: number): Promise<void> {
+        try {
+            const user = await prisma.user.findUnique({
+                where: {
+                    id: userId,
+                },
+            });
+            if (user) {
+                await prisma.user.delete({
+                    where: {
+                        id: user.id,
+                    },
+                });
+            } else {
+                console.error(`User with ID ${userId} not found.`);
+            }
+        } catch (error) {
+            console.error("Error deleting user:", error);
+            throw new Error("Failed to delete user");
+        }
+    }
 }
---
+++ src/routes/api/user.ts
@@ -0,0 +1,61 @@
+import express, { Request, Response } from 'express';
+import {User} from "../../classes/user";
+
+const router = express.Router();
+
+router.get("/", async (req: Request, res: Response) => {
+    // http://127.0.0.1:3000/api/users/
+    try {
+        const users: User[] = await User.getAllUsers();
+        return res.status(200).send(users);
+    } catch (e) {
+        console.error("Error fetching products:", e);
+        res.status(500).send('Internal server error');
+    }
+});
+
+router.get("/:id", async (req: Request, res: Response) => {
+    try {
+        const userId = parseInt(req.params.id, 10);
+        const user = await User.getUser(userId);
+        return res.status(200).send(user);
+    } catch (e) {
+        console.error("Error fetching users:", e);
+        res.status(500).send('Internal server error');
+    }
+});
+
+router.post("/", async (req, res) => {
+    const { name, price } = req.body;
+    try {
+        const newUser = await User.createUser(name, price);
+        return res.status(201).send(newUser);
+    } catch (e) {
+        res.status(500).send('Internal server error');
+    }
+});
+
+router.put("/:id", async (req, res) => {
+    const { name, email } = req.body;
+    try {
+        const userId = parseInt(req.params.id, 10);
+        const user = await User.updateUser(userId, name, email);
+        return res.status(200).send(user);
+    } catch (e) {
+        res.status(500).send('Internal server error');
+    }
+});
+
+router.delete("/:id", async (req: Request, res: Response) => {
+    try {
+        const userId = parseInt(req.params.id, 10);
+        await User.deleteUser(userId);
+        return res.status(204).send();
+    } catch (e) {
+        console.error("Error deleting user:", e);
+        res.status(500).send('Internal server error');
+    }
+});
+
+
+export default router;
