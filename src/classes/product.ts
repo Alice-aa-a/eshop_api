66402f2aec5717bd7413e94b6e9f125fc814c4f5
Alice <a.aliceabraham@gmail.com>
2023-12-21 13:57:33 +0100

    CRUD products

--- src/classes/product.ts
+++ src/classes/product.ts
@@ -25,6 +25,28 @@ export class Product {
             throw new Error("Failed to fetch products");
         }
     }
+    public static async getProduct(productId: number): Promise<Product | null> {
+        try {
+            const existingProduct = await prisma.product.findUnique({
+                where: {
+                    id: productId,
+                },
+            });
+            if (existingProduct) {
+                return {
+                    id: existingProduct.id,
+                    name: existingProduct.name,
+                    price: existingProduct.price
+                };
+            } else {
+                console.error(`Product with ID ${productId} not found.`);
+                return null;
+            }
+        } catch (error) {
+            console.error("Error creating product:", error);
+            throw new Error("Failed to create product");
+        }
+    }
     public static async createProduct(name: string, price: number): Promise<Product> {
         try {
             const newProduct = await prisma.product.create(
@@ -42,6 +64,59 @@ export class Product {
         } catch (error) {
             console.error("Error creating product:", error);
             throw new Error("Failed to create product");
+        }
+    }
+    public static async updateProduct(id: number, name: string, price: number): Promise<Product | null> {
+        try {
+            const existingProduct = await prisma.product.findUnique({
+                where: {
+                    id: id,
+                },
+            });
+            if (existingProduct) {
+                const updatedProduct = await prisma.product.update({
+                    where: {
+                        id: id,
+                    },
+                    data: {
+                        name: name,
+                        price: price,
+                    },
+                });
+                return {
+                    id: updatedProduct.id,
+                    name: updatedProduct.name,
+                    price: updatedProduct.price,
+                };
+            } else {
+                console.error(`Product with ID ${id} not found.`);
+                return null;
+            }
+        } catch (error) {
+            console.error("Error updating product:", error);
+            throw new Error("Failed to update product");
+        }
+    }
+
+    public static async deleteProduct(productId: number): Promise<void> {
+        try {
+            const product = await prisma.product.findUnique({
+                where: {
+                    id: productId,
+                },
+            });
+            if (product) {
+                await prisma.product.delete({
+                    where: {
+                        id: product.id,
+                    },
+                });
+            } else {
+                console.error(`Product with ID ${productId} not found.`);
+            }
+        } catch (error) {
+            console.error("Error deleting product:", error);
+            throw new Error("Failed to delete product");
         }
     }
 }
--- src/routes/api/product.ts
+++ src/routes/api/product.ts
@@ -14,6 +14,16 @@ router.get("/", async (req: Request, res: Response) => {
     }
 });
 
+router.get("/:id", async (req: Request, res: Response) => {
+    try {
+        const productId = parseInt(req.params.id, 10);
+        const product= await Product.getProduct(productId);
+        return res.status(200).send(product);
+    } catch (e) {
+        console.error("Error fetching products:", e);
+        res.status(500).send('Internal server error');
+    }
+});
 
 router.post("/", async (req, res) => {
     const { name, price } = req.body;
@@ -21,6 +31,28 @@ router.post("/", async (req, res) => {
         const newProduct = await Product.createProduct(name, price);
         return res.status(201).send(newProduct);
     } catch (e) {
+        res.status(500).send('Internal server error');
+    }
+});
+
+router.put("/:id", async (req, res) => {
+    const { name, price } = req.body;
+    try {
+        const productId = parseInt(req.params.id, 10);
+        const product = await Product.updateProduct(productId, name, price);
+        return res.status(200).send(product);
+    } catch (e) {
+        res.status(500).send('Internal server error');
+    }
+});
+
+router.delete("/:id", async (req: Request, res: Response) => {
+    try {
+        const productId = parseInt(req.params.id, 10);
+        await Product.deleteProduct(productId);
+        return res.status(204).send();
+    } catch (e) {
+        console.error("Error deleting product:", e);
         res.status(500).send('Internal server error');
     }
 });
